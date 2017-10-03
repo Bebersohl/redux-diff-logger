98553308e634b4d6b115fced5f0a0b6d53119a09
Alexander Raich <alexander.raich@elca.ch>
2017-10-03 11:09:47 +0200

    Executed build.

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 Object.defineProperty(exports, "__esModule", {
-  value: true
+	value: true
 });
 
 var _diff = require('diff');
@@ -21,45 +21,65 @@ var _detectBrowser2 = _interopRequireDefault(_detectBrowser);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function (store) {
-  return function (next) {
-    return function (action) {
-      var currentState = store.getState();
-      var result = next(action);
-      var nextState = store.getState();
-      var diff = (0, _diff.diffJson)(currentState, nextState);
-      var positive = 0;
-      var negative = 0;
+	return function (next) {
+		return function (action) {
+			var currentState = store.getState();
+			var result = next(action);
+			var nextState = store.getState();
+			var diff = (0, _diff.diffJson)(currentState, nextState);
+			var positive = 0;
+			var negative = 0;
 
-      diff.forEach(function (part) {
-        if (part.added) {
-          part.color = 'green';
-          positive += 1;
-        } else if (part.removed) {
-          part.color = 'red';
-          negative += 1;
-        } else {
-          part.color = 'grey';
-        }
-      });
+			diff.forEach(function (part) {
+				if (part.added) {
+					part.color = 'green';
+					positive += 1;
+				} else if (part.removed) {
+					part.color = 'red';
+					negative += 1;
+				} else {
+					part.color = 'grey';
+				}
+			});
 
-      if (_detectNode2.default) {
-        console.log(_chalk2.default.white(action.type), _chalk2.default.green('+' + positive), _chalk2.default.red('-' + negative));
-        diff.forEach(function (part) {
-          process.stderr.write(_chalk2.default[part.color](part.value));
-        });
-        console.log(_os.EOL + '\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014');
-      } else {
-        var log = _detectBrowser2.default.name === 'chrome' ? console.groupCollapsed : console.log;
-        log(action.type + ' %c+' + positive + ' %c-' + negative, 'color: green', 'color: red');
-        diff.forEach(function (part) {
-          console.log('%c' + part.value, 'color: ' + part.color);
-        });
-        if (_detectBrowser2.default.name === 'chrome') {
-          console.groupEnd();
-        }
-      }
+			var logForChrome = function logForChrome() {
+				console.groupCollapsed(action.type + ' %c+' + positive + ' %c-' + negative, 'color: green', 'color: red');
+				diff.forEach(function (part) {
+					console.log('%c' + part.value, 'color: ' + part.color);
+				});
+				console.groupEnd();
+			};
+			var logForIE = function logForIE() {
+				console.groupCollapsed(action.type + ' +' + positive + ' -' + negative);
+				diff.forEach(function (part) {
+					console.log('' + part.value);
+				});
+				console.groupEnd();
+			};
+			var logForOther = function logForOther() {
+				console.log(action.type + ' +' + positive + ' -' + negative);
+				diff.forEach(function (part) {
+					console.log('%c' + part.value, 'color: ' + part.color);
+				});
+			};
+			if (_detectNode2.default) {
+				console.log(_chalk2.default.white(action.type), _chalk2.default.green('+' + positive), _chalk2.default.red('-' + negative));
+				diff.forEach(function (part) {
+					process.stderr.write(_chalk2.default[part.color](part.value));
+				});
+				console.log(_os.EOL + '\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014');
+			} else {
+				console.log('browser is ' + _detectBrowser2.default.name);
+				if (_detectBrowser2.default.name === 'chrome') {
+					logForChrome();
+				} else if (_detectBrowser2.default.name === 'ie') {
+					logForIE();
+				} else {
+					logForOther();
+				}
+			}
 
-      return result;
-    };
-  };
+			return result;
+		};
+	};
 };
